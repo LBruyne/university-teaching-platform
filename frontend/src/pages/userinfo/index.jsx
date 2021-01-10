73700a95d90f79e940b25148d7593da7e98daf5b
Llux-C <65882191+Llux-C@users.noreply.github.com>
2021-01-10 19:37:43 +0800

    Add files via upload

--- frontend/src/pages/userinfo/index.jsx
+++ frontend/src/pages/userinfo/index.jsx
@@ -4,6 +4,7 @@ import { Card, Row, Col, Divider, Avatar, Upload, Button, Tabs, Input} from "ant
 import { UserOutlined } from '@ant-design/icons';
 import { UploadOutlined, InboxOutlined } from '@ant-design/icons';
 
+import Login from '../../components/login/index.jsx'
 
 const { TabPane } = Tabs;
 
@@ -12,7 +13,112 @@ function callback(key) {
 }
 
 class UserInfoPage extends React.Component {
+
+    state = {
+        isLogin: false,
+        username: "",
+        type:"",
+        major:"",
+        id:"",
+        email:"",
+        course:"",
+        url:"",
+        nickname:""
+    }
+
+    componentWillMount() {
+        let isLogin = localStorage.getItem("isLogin")
+        if(isLogin=="true"){
+            this.state.isLogin = true;
+            this.state.username = localStorage.getItem("username")
+            this.state.type = localStorage.getItem("type")
+        }
+        this.attemptFetch()
+        console.log(this.state.username)
+        console.log(this.state.type)
+    }
+
+    attemptFetch = () =>
+    {
+        let url = "http://localhost:5000/userInfo?" + "userName=" + this.state.username + "&" +
+            "type=" + this.state.type
+        let fileurl="http://localhost:5000/fetchFile?" + "userName=" + this.state.username + "&" +
+            "type=1"
+        // 从外部取数据
+        fetch(url, {
+                method: "GET",
+                mode: "cors",
+            }
+        )
+        .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                if(this.state.type=="stu")
+                {
+                    this.setState({
+                        id: data["id"],
+                        major: data["major"],
+                        email: data["email"],
+
+                    })
+                }
+                else if(this.state.type=="ins")
+                {
+                    this.setState({
+                        id: data["id"],
+                        email: data["email"],
+                        major:data["department"],
+
+                    })
+                }
+                else if(this.state.type=="ta")
+                {
+                    this.setState({
+                        id: data["teacherId"],
+                        course: data["courseName"],
+
+                    })
+                }
+
+                console.log(this.state.major)
+            })
+
+        fetch(fileurl, {
+                method: "GET",
+                mode: "cors",
+            }
+        )
+        .then(response => response.json())
+            .then(data => {
+            console.log(data)
+                if(this.state.type!="ta")
+                {
+                    this.setState({
+                        url:data["url"]
+                    })
+                }
+            })
+    }
+
+    attemptPost=()=>
+    {
+        let url = "http://localhost:5000/modifyInfo"
+        fetch(url, {
+                method: "POST",
+                mode: "cors",
+                body:'userName=${this.state.username}&passWD=""&nickName=${this.state.nickname}'
+            }
+        ).then((response)=>
+        {
+            return response.json()
+        }).then((data)=>
+        {
+            console.log(data)
+        })
+    }
+
     render() {
+
         return (
 
             <>
@@ -24,14 +130,14 @@ class UserInfoPage extends React.Component {
                     <Col span={16}>
                         <Tabs defaultActiveKey="1" onChange={callback}>
                             <TabPane tab="信息查看" key="1" size="large">
-                                <Divider plain>基本信息</Divider>
+                                <Divider plain >基本信息</Divider>
                                 <Row>
                                     <Col span={2}></Col>
                                     <Col span={8}>
                                         姓名
                                     </Col>
                                     <Col>
-                                        刘小米
+                                        {this.state.username}
                                     </Col>
                                 </Row>
                                 <br/>
@@ -42,7 +148,7 @@ class UserInfoPage extends React.Component {
                                     </Col>
                                     <Col>
                                         <div>
-                                            <Avatar size={128} icon={<UserOutlined/>}/>
+                                              <el-avatar src={this.state.url}></el-avatar>
                                         </div>
                                     </Col>
                                 </Row>
@@ -53,7 +159,7 @@ class UserInfoPage extends React.Component {
                                         平台角色
                                     </Col>
                                     <Col>
-                                        学生
+                                        {this.state.type}
                                     </Col>
                                 </Row>
                                 <br/>
@@ -63,123 +169,73 @@ class UserInfoPage extends React.Component {
                                         人员编号
                                     </Col>
                                     <Col>
-                                        3180000000
+                                        {this.state.id}
                                     </Col>
                                 </Row>
                                 <br/>
                                 <Row>
                                     <Col span={2}></Col>
+                                    {this.state.type=="ins"&&
                                     <Col span={8}>
                                         系级
                                     </Col>
-                                    <Col>
-                                        计算机科学与技术学院
+                                    }
+                                    {this.state.type=="stu"&&
+                                    <Col span={8}>
+                                        系级
                                     </Col>
+                                    }
+                                    {this.state.type!="ta"&&
+                                    <Col>
+                                        {this.state.major}
+                                    </Col>
+                                    }
                                 </Row>
                                 <br/>
+                                {this.state.type!="ta"&&
                                 <Divider plain>账号绑定</Divider>
+                                }
+                                {this.state.type!="ta"&&
                                 <Row>
                                     <Col span={2}></Col>
                                     <Col span={8}>
                                         Email
                                     </Col>
                                     <Col>
-                                        3180000000@zju.edu.cn
+                                        {this.state.email}
                                     </Col>
                                 </Row>
+                                }
                                 <br/>
                                 <Row>
                                     <Col span={2}></Col>
                                     <Col span={8}>
-                                        手机号码
+                                        手机号
                                     </Col>
                                     <Col>
-                                        13700000000
+                                        {this.state.nickname}
                                     </Col>
                                 </Row>
                             </TabPane>
                             <TabPane tab="信息修改" key="2" size="large">
-                                <Divider plain>基本信息</Divider>
-                                <Row>
-                                    <Col span={2}></Col>
-                                    <Col span={8}>
-                                        姓名
-                                    </Col>
-                                    <Col>
-                                        <Input placeholder="刘小米"/>
-                                    </Col>
-                                </Row>
-                                <br/>
-                                <Row>
-                                    <Col span={2}></Col>
-                                    <Col span={8}>
-                                        头像
-                                    </Col>
-                                    <Col>
-                                        <div>
-                                            <Upload name="logo" action="/upload.do" listType="picture">
-                                                <Button icon={<UploadOutlined/>}>点击上传头像</Button>
-                                            </Upload>
-                                        </div>
-                                        <br/>
-                                        <div>
-                                            <Avatar size={128} icon={<UserOutlined/>}/>
-                                        </div>
-                                    </Col>
-                                </Row>
-                                <br/>
-                                <Row>
-                                    <Col span={2}></Col>
-                                    <Col span={8}>
-                                        平台角色
-                                    </Col>
-                                    <Col>
-                                        <Input placeholder="学生"/>
-                                    </Col>
-                                </Row>
-                                <br/>
-                                <Row>
-                                    <Col span={2}></Col>
-                                    <Col span={8}>
-                                        人员编号
-                                    </Col>
-                                    <Col>
-                                        <Input placeholder="3180000000"/>
-                                    </Col>
-                                </Row>
-                                <br/>
-                                <Row>
-                                    <Col span={2}></Col>
-                                    <Col span={8}>
-                                        系级
-                                    </Col>
-                                    <Col>
-                                        <Input placeholder="计算机科学与技术学院"/>
-                                    </Col>
-                                </Row>
-                                <br/>
+                                {this.state.type!="ta"&&
                                 <Divider plain>账号绑定</Divider>
+                                }
+                                {this.state.type!="ta"&&
                                 <Row>
                                     <Col span={2}></Col>
                                     <Col span={8}>
-                                        Email
+                                        手机号
                                     </Col>
                                     <Col>
-                                        <Input placeholder="33180000000@zju.edu.cn"/>
+                                        <Input placeholder={this.state.nickname}/>
                                     </Col>
                                 </Row>
+                                }
                                 <br/>
-                                <Row>
-                                    <Col span={2}></Col>
-                                    <Col span={8}>
-                                        手机号码
-                                    </Col>
-                                    <Col>
-                                        <Input placeholder="13700000000"/>
-                                    </Col>
-                                </Row>
-                                <br/>
-                                <Button>确定修改</Button>
+                                <div style={{textAlign:'center'}}>
+                                    <Button onClick={()=>this.attemptPost()} >确定修改</Button>
+                                </div>
                             </TabPane>
                         </Tabs>
                     </Col>
