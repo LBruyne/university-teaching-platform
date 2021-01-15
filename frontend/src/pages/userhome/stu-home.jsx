eb240554d517842218d40760ea9e6d7c3361236c
ycyorket <50347358+ycyorket@users.noreply.github.com>
2021-01-15 19:44:54 +0800

    Update stu-home.jsx

--- frontend/src/pages/userhome/stu-home.jsx
+++ frontend/src/pages/userhome/stu-home.jsx
@@ -1,96 +1,125 @@
 import React from 'react';
 import klee from '../../assets/images/user/klee.jpg';
+import Notes from './notes';
 
 class StuHome extends React.Component{
     constructor(props) {
         super(props);
+        let type = localStorage.getItem("type") || 'stu';
+        let id = localStorage.getItem("username") || 'Klee';
         this.state = {
-            user_type: "stu",
-            user_id: "u123",
+            get_info_url: "http://localhost:5000/userinfo",
+            course_url: "http://localhost:5000/studyCourse",
+            user_type: type,
+            user_id: id,
+            chosen_item: "notice",
+            courses: [],
+            notices: [{
+                title: "软件需求规格说明书（重点）作业发布",
+                time: "2020-12-11 17:21 p.m.",
+                due: "已过期",
+                text: "请大家注意截止日期，及时提交",
+            },{
+                title: "软件需求工程组会纪要提交提醒",
+                time: "2020-12-07 19:21 p.m.",
+                due: "已过期",
+                text: "还没有交组会纪要的小组请赶快提交",
+            },{
+                title: "软件需求工程开课公告",
+                time: "2020-09-13 09:19 a.m.",
+                due: "已过期",
+                text: "软件需求工程课程即将于9月16日（星期三）开课，请大家提前做好准备",
+            }]
         };
     }
+    
+    componentWillMount(){
+        fetch(this.state.course_url + '?userName=' + this.state.user_id, {
+                method: "GET",
+                mode: "cors",
+            }
+        )
+        .then(response => response.json())
+        .then(data => {
+            this.setState({courses: data.length? data: [{'课程标识符': 1, '课程名称': '软件需求工程'}, 
+            {'课程标识符': 2, '课程名称': '软件工程管理'}, 
+            {'课程标识符': 3, '课程名称': '软件质量保证与测试'}, 
+            {'课程标识符': 4, '课程名称': '服务科学导论'}, 
+            {'课程标识符': 5, '课程名称': '软件工程基础'}]})
+            console.log(this.state.course_url + '?userName=' + this.state.id)
+        }).catch(function (e) {
+            console.log(e);
+        });
+    }
+    
     get_avatar(){
         return klee;
     }
+    
     get_name(){
-        //@TODO
-        let name = localStorage.getItem('username');
-        return "Klee";
+        return this.state.user_id
     }
-    go2_notice_details(){
-        alert("details is in building...");
-    }
+    
     get_notices(){
-        let notices = [
-            {
-                'title': 'not1',
-                'content': '课程 软件需求工程 的作业 软件需求工程课程总结 已于2021.01.10 10:21开放'
-            },
-            {
-                'title': 'not2',
-                'content': '课程 软件需求工程 的作业 组内评分表 已于2021.01.10 10:19开放'
-            },
-            {
-                'title': 'not3',
-                'content': '课程 软件需求工程 的作业 项目总结陈述PPT 已于2021.01.10 10:20开放'
-            },
-            {
-                'title': 'not4',
-                'content': '课程 软件需求工程 的作业 项目总结报告 提交即将于今天截止'
-            },
-        ];
+        let notices = this.state.notices
         return (
-            <div class={"notices"}>
-                {notices.map(item =>(
-                    <li class={'notice-item'} key={item}>
-                        <div class={'item-title'}
-                        onClick={this.go2_notice_details}>
-                            {item['title']}
+            <div class={"notices-list"}>
+                {notices.map(item=>(
+                    <div class={'notice-item'} key={item}>
+                        <div class={'notice-header'}>
+                            <div class={'notice-header-left'}>
+                                <div class={'notice-logo'}></div>
+                            </div>
+                            <div class={'notice-header-right'}>
+                                <div class={'notice-header-right-top'}>
+                                    <div class={'notice-title'}>{item.title}</div>
+                                    <div class={'notice-due'}>{item.due}</div>
+                                </div>
+                                <div class={'notice-header-right-bot'}>
+                                    <div class={'notice-time'}>{item.time}</div>
+                                    <div class={'notice-detail'}>查看详情</div>
+                                </div>
+                            </div>
                         </div>
-                        <div class={'item-content'}>{item['content']}</div>
-                    </li>
+                        <div class={'notice-text'}>{item.text}</div>
+                    </div>
                 ))}
             </div>
         );
     }
-    get_courses(){
-        let courses = [
-            {
-                'id': 1,
-                'name': '软件需求工程',
-                'info': '',
-            },
-            {
-                'id': 2,
-                'name': '软件工程管理',
-                'info': '',
-            },
-            {
-                'id': 3,
-                'name': '软件质量测试与保证',
-                'info': '',
-            },
-            {
-                'id': 4,
-                'name': '服务科学导论',
-                'info': '',
-            },
-        ]
+    get_courses = () => {
+        let courses = this.state.courses
         return (
             <div class={"courses-list"}>
                 {courses.map(item=>(
-                    <li class={'course-item'}
-                        key={item}
-                        onClick={()=>{
-                            window.location.href = '/user/course?course='+item['id']
-                        }}
-                    >
-                        {item['name']}</li>
+                    <div class={'course-item'} key={item} onClick={()=>{window.location.href = '/user/course?course='+item['课程标识符']}}>
+                        <div class='course-title'>
+                            <div class='course-title-text'>{item['课程名称']}</div>
+                        </div>
+                    </div>
                 ))}
             </div>
         );
     }
-
+    
+    show_center_title(){
+        if(this.state.chosen_item == "notes")
+            return <div class={'center-title-bar'}>学习笔记 Notes</div>
+        else
+            return <div class={'center-title-bar'}>公告通知 Notices</div>
+    }
+    
+    show_center_content(){
+        if(this.state.chosen_item == "notes")
+            return <Notes></Notes>
+        else
+            return this.get_notices()
+    }
+    
+    check_item(item){
+        return this.state.chosen_item == item? "item": "otherItem"
+    }
+    
     render(){
         return (
             <div class="uhp">
@@ -100,21 +129,19 @@ class StuHome extends React.Component{
                         <div>{this.get_name()}</div>
                     </div>
                     <div class={"menu"}>
-                        <div class={"item"}
-                        onClick={()=>{
-                            window.location.href = '/user/home/notes';
-                        }}>学习笔记</div>
-                        <div class={"item"}>公告</div>
+                        <div className={this.check_item("notes")} onClick={()=>{this.setState({chosen_item: "notes", })}}>学习笔记</div>
+                        <div className={this.check_item("notice")} onClick={()=>{this.setState({chosen_item: "notice", })}}>公告</div>
+                        <div className={this.check_item("setting")} onClick={()=>{this.setState({chosen_item: "setting", });window.location.href = '/user/setting'}}>设置</div>
                     </div>
                 </div>
 
                 <div class="uhp-center">
-                    <div class={"notices-title-bar"}>notice</div>
-                    {this.get_notices()}
+                    {this.show_center_title()}
+                    {this.show_center_content()}
                 </div>
 
                 <div class="uhp-right">
-                    <div class={"courses-title-bar"}>courses</div>
+                    <div class={"courses-title-bar"}>在学课程 COURSES</div>
                     {this.get_courses()}
                 </div>
             </div>
