edff71e85ebee8d8e43ba6bd97b0b3cab1795513
ycyorket <50347358+ycyorket@users.noreply.github.com>
2021-01-15 19:41:50 +0800

    Update index.jsx

--- frontend/src/pages/course/index.jsx
+++ frontend/src/pages/course/index.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import StuCourse from "./stu-course";
 import TchCourse from "./tch-course";
-import TaCourse from "./ta-course";
 
 class CoursePage extends React.Component{
     constructor(props) {
         super(props);
 
         this.state = {
-            utype: 'stu',
+            utype: localStorage.getItem("type"),
         }
     }
 
     render(){
-
         return (
             <div>
                 {this.state.utype == 'stu' && <StuCourse />}
                 {this.state.utype == 'tch' && <TchCourse />}
-                {this.state.utype == 'ta'  && <TaCourse />}
+                {this.state.utype == 'ta'  && <TchCourse />}
             </div>
         )
     }
